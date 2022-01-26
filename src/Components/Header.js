81280887f6c81c565c047d38d04b0eae2f84d5e9
Prith Kami <prithvi40201@gmail.com>
2022-01-26 16:19:40 +0530

    commit 1

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+function Header() {
+  return (<div className='header'>
+      <div className="logoContainer">
+          <img src={punklogo} className='punklogo' alt="" />
+      </div>
+  </div>
+  )}
+
+export default Header;
